38563b3ae4d3c529d099b4ab2eff79db88739bd9
窦克兴 <doukexing@doukexingdeMacBook-Pro.local>
2017-04-10 21:27:54 +0800

    增加登录验证admin功能，增加后台layout界面，user增加is_admin属性，增加admin用户seeds种子

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,11 @@
 class Admin::ProductsController < ApplicationController
 
+  layout "admin"
+  before_action :authenticate_user!
+  before_action :admin_required
+
+
+
   def index
     @products = Product.all
   end
@@ -26,7 +32,7 @@ class Admin::ProductsController < ApplicationController
       render :edit
     end
   end
-  
+
 
   private
   def product_params
