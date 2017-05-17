e8b9bb19d4df71fc123ab62efebb57a2b88232b3
窦克兴 <doukexing@doukexingdeMacBook-Pro.local>
2017-05-17 14:36:17 +0800

    add payment_method function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,6 +29,20 @@ class OrdersController < ApplicationController
     @product_lists = @order.product_lists
   end
 
+  def pay_with_alipay
+    @order = Order.find_by_token(params[:id])
+    @order.set_with_payment!("alipay")
+    @order.pay!
+    redirect_to order_path(@order.token),notice:"使用支付宝已经付款"
+  end
+
+  def pay_with_wechat
+    @order = Order.find_by_token(params[:id])
+    @order.set_with_payment!("wechat")
+    @order.pay!
+    redirect_to order_path(@order.token),notice:"使用微信已经付款"
+  end
+
   private
   def order_params
     params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
